91f3cd67d8e6a40b2c1988ab28b89de3903d34d5
Andy Hsiang <xenogt@gmail.com>
2017-07-15 21:46:13 -0400

    ran pep8 and updated all errors

--- report_api.py
+++ report_api.py
@@ -1,93 +1,86 @@
 #! /usr/bin/python
 
-import psycopg2, bleach
+import psycopg2
+import bleach
 
 DBNAME = "news"
 
-'''
-runs the query to fetch top 3 articles' title that have most
-views, prints the resulting titles with most viewed title first
-'''
+
 def get_top_3_viewed_articles():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(
-    "select title as title, num as count from "+
-      "(select title, slug, count(*) as num "+
-      "from log a join articles b on substring(a.path, 10)=b.slug "+
-      "group by b.slug, b.title order by num desc limit 3) "+
-    "result;")
-  articles = c.fetchall()
-  print("")
-  print("----------------------------------------------------")
-  for i in articles:
-    print(i[0]+"  ---  "+str(i[1])+" views")
-  print("")
-  db.close()
-  return articles
+    '''
+    runs the query to fetch top 3 articles' title that have most
+    views, prints the resulting titles with most viewed title first
+    '''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select title as title, num as count from " +
+        "(select title, slug, count(*) as num " +
+        "from log a join articles b on substring(a.path, 10)=b.slug " +
+        "group by b.slug, b.title order by num desc limit 3) " +
+        "result;")
+    articles = c.fetchall()
+    print("")
+    print("----------------------------------------------------")
+    for i in articles:
+        print(i[0]+"  ---  "+str(i[1])+" views")
+    print("")
+    db.close()
+    return articles
 
-'''
-runs the query to fetch authors' names and combined number of views
-on articles per arthor. Prints the resulting data, authors with most
-combined views of articles would be displayed on top.
-'''
+
 def get_most_popular_author_list():
-  """Return all posts from the 'database', most recent first."""
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(
-    "select name, result_author.num "+
-    "from authors, "+
-      "(select author, count(*) as num "+
-      "from log a join articles b on substring(a.path, 10)=b.slug "+
-      "group by b.author "+
-      "order by num desc)"+
-    "as result_author "+
-    "where id=result_author.author;")
-  authors = c.fetchall()
-  print("")
-  print("----------------------------------------------------")
-  for i in authors:
-    print(i[0]+"  ---  "+str(i[1])+" views")
-  print("")
-  db.close()
-  return authors
+    '''
+    runs the query to fetch authors' names and combined number of views
+    on articles per arthor. Prints the resulting data, authors with most
+    combined views of articles would be displayed on top.
+    '''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select name, result_author.num " +
+        "from authors, " +
+        "(select author, count(*) as num " +
+        "from log a join articles b on substring(a.path, 10)=b.slug " +
+        "group by b.author " +
+        "order by num desc)" +
+        "as result_author " +
+        "where id=result_author.author;")
+    authors = c.fetchall()
+    print("")
+    print("----------------------------------------------------")
+    for i in authors:
+        print(i[0]+"  ---  "+str(i[1])+" views")
+    print("")
+    db.close()
+    return authors
 
-'''
-runs query to find days that has more than 1% of access result in
-error. Prints the information on screen.
-'''
+
 def get_days_result_in_1percent_error():
-  db = psycopg2.connect(database=DBNAME)
-  c = db.cursor()
-  c.execute(
-    "select c.t, c.errors "+
-    "from"+ 
-    "(select "+
-      "a.t1::timestamp::date as t, "+
-      "(a.error::decimal/b.total)*100 as errors "+
-      "from "+
-        "(select "+
-          "time::timestamp::date as t1, "+
-          "count(time::timestamp::date) as error "+
-          "from log "+
-          "where status like '%404%' "+
-          "group by time::timestamp::date"+
-        ") a, "+
-        "(select "+
-          "time::timestamp::date as t2, "+
-          "count(time::timestamp::date) as total "+
-          "from log "+
-          "group by time::timestamp::date"+
-          ") b "+
-        "where a.t1=b.t2"+
-    ") c where c.errors > 1;")
-  days = c.fetchall()
-  print("")
-  print("----------------------------------------------------")
-  for i in days:
-    print(str(i[0])+"  ---  "+str(round(i[1], 2))+"% errors")
-  print("")
-  db.close()
-  return days
+    '''
+    runs query to find days that has more than 1% of access result in
+    error. Prints the information on screen.
+    '''
+    db = psycopg2.connect(database=DBNAME)
+    c = db.cursor()
+    c.execute(
+        "select c.t, c.errors from" +
+        "(select a.t1::timestamp::date as t, " +
+        "(a.error::decimal/b.total)*100 as errors " +
+        "from (select " +
+        "time::timestamp::date as t1, " +
+        "count(time::timestamp::date) as error " +
+        "from log where status like '%404%' " +
+        "group by time::timestamp::date) a, " +
+        "(select time::timestamp::date as t2, " +
+        "count(time::timestamp::date) as total " +
+        "from log group by time::timestamp::date) b " +
+        "where a.t1=b.t2) c where c.errors > 1;")
+    days = c.fetchall()
+    print("")
+    print("----------------------------------------------------")
+    for i in days:
+        print(str(i[0])+"  ---  "+str(round(i[1], 2))+"% errors")
+    print("")
+    db.close()
+    return days
--- report_tool.py
+++ report_tool.py
@@ -1,16 +1,16 @@
 import report_api as api
 
-'''
-This method presents the initial screen when running the application. It 
-will show the user a list of project questions and allows the user to
-select the item which will triggers the corresponding query to run to return
-useful information.
-'''
-def show_menu():
-    leave = False
 
+def show_menu():
+    '''
+    This method presents the initial screen when running the application. It
+    will show the user a list of project questions and allows the user to
+    select the item which will triggers the corresponding query to run to
+    return useful information.
+    '''
+    leave = False
     # loops forever until leave === True
-    while(leave==False):
+    while(!leave):
         Q1 = "1 - What are the most popular three articles of all time?"
         Q2 = "2 - Who are the most popular article authors of all time?"
         Q3 = "3 - On which days did more than 1% of requests lead to errors?"
@@ -31,15 +31,15 @@ def show_menu():
         print("")
 
         if str(a) == "1":
-            print("REPORT "+Q1)
+            print("REPORT " + Q1)
             api.get_top_3_viewed_articles()
             raw_input(continue_msg)
         elif str(a) == "2":
-            print("REPORT "+Q2)
+            print("REPORT " + Q2)
             api.get_most_popular_author_list()
             raw_input(continue_msg)
         elif str(a) == "3":
-            print("REPORT "+Q3)
+            print("REPORT " + Q3)
             api.get_days_result_in_1percent_error()
             raw_input(continue_msg)
         elif str(a) == "4":
