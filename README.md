db01199f6c797e7ecf7fe2b91d27b84a0f0985c6
Andy Hsiang <andyhsiang@Andys-MBP.attlocal.net>
2017-07-15 20:25:24 -0400

    added code to answer all 3 questions

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Logs-Analysis
+
+## 1. What are the most popular three articles of all time? 
+/code select title, slug, count(*) as num from log a join articles b on substring(a.path, 10)=b.slug group by b.slug, b.title order by num desc limit 3;
+
+ ###candidate-is-jerk         | 338647
+ ###bears-love-berries        | 253801
+ ###bad-things-gone           | 170098
+
+## 2. Who are the most popular article authors of all time?
+
+select authors.name from authors as authors, (select author from (select author, count(*) as num from log a join articles b on substring(a.path, 10)=b.slug group by b.author order by num desc) as result) as result_author where authors.id=result_author.author;
+
+## 3. On which days did more than 1% of requests lead to errors?
---
+++ report_tool.py
@@ -0,0 +1,43 @@
+import report_api as api
+import sys
+
+def show_menu():
+    leave = False
+    while(leave==False):
+        Q1 = "1 - What are the most popular three articles of all time?"
+        Q2 = "2 - Who are the most popular article authors of all time?"
+        Q3 = "3 - On which days did more than 1% of requests lead to errors?"
+        continue_msg = "press any key to continue ... "
+
+        print("----------------------------------------------------")
+        print("                 Log Reporting Tool     ")
+        print("----------------------------------------------------")
+        print("")
+        print("Select the corresponding number to view report")
+        print("")
+        print(Q1)
+        print(Q2)
+        print(Q3)
+        print("4 - exit")
+        print("")
+        a = raw_input("Please select: ")
+        print("")
+        if str(a) == "1":
+            print("REPORT "+Q1)
+            api.get_top_3_viewed_articles()
+            raw_input(continue_msg)
+        elif str(a) == "2":
+            print("REPORT "+Q2)
+            api.get_most_popular_author_list()
+            raw_input(continue_msg)
+        elif str(a) == "3":
+            print("REPORT "+Q3)
+            api.get_days_result_in_1percent_error()
+            raw_input(continue_msg)
+        elif str(a) == "4":
+            print("exiting ... BYE")
+            leave = True
+        else:
+            raw_input("invalid input, press any key to continue ...")
+
+show_menu()
